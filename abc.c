75a59be944cbcabdcfc68fe005bba0ec32363b29
Abhay Kumar Lodhi <abhaylodhi24@gmail.com>
2021-12-11 14:36:34 +0530

    Commit Message

---
+++ abc.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+
+int main(){
+    int length ,breadth;
+    printf("enter the length of rectangle \n");
+    scanf("%d",&length);
+      printf("enter the breadth of rectangle \n");
+    scanf("%d",&breadth);
+    printf("Area of rectangle is %d",length*breadth);
+    return 0;
+}
